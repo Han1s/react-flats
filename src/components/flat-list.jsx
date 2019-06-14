f475be023473316b117c63d4897c1e9cdbf45dad
Jan Ranostaj <HonzaRanostaj@gmail.com>
2019-06-14 16:51:49 +0200

    dynamic flats

--- src/components/flat-list.jsx
+++ src/components/flat-list.jsx
@@ -3,15 +3,15 @@ import Flat from './flat';
 
 class FlatList extends Component {
   render() {
-        console.log(this.props.flats);
     return (
       <div className="flat-list">
-        <Flat></Flat>
-        <Flat></Flat>
-        <Flat></Flat>
-        <Flat></Flat>
-        <Flat></Flat>
-        <Flat></Flat>
+        {
+          this.props.flats.map((flat) => {
+            return (
+              <Flat flatInfo={flat} key={flat.lat}></Flat>
+            )
+          })
+        }
       </div>
     );
   }
