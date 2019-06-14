82d25383c9ceacca64931207cf5d6c070a2cc8b7
Jan Ranostaj <HonzaRanostaj@gmail.com>
2019-06-14 16:28:54 +0200

    default flat styles

--- src/components/flat-list.jsx
+++ src/components/flat-list.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Flat from './flat';
 
 class FlatList extends Component {
   render() {
         console.log(this.props.flats);
     return (
       <div className="flat-list">
+        <Flat></Flat>
+        <Flat></Flat>
+        <Flat></Flat>
       </div>
     );
   }
--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 
 class Flat extends Component {
-  return(
-    <div class="card" style="background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url('https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg';);">
-      <div class="card-category">150 EUR</div>
-      <div class="card-description">
+render() {
+    return (
+      <div className="card" style="background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url('https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg';);">
+        <div className="card-category">150 EUR</div>
+        <div className="card-description">
         <h2>Super 60m2 in trendy neighborhood!</h2>
+        </div>
+        <a className="card-link" href="#"></a>
       </div>
-      <a class="card-link" href="#"></a>
-    </div>
-  )
+    )
+  }
 }
 
 
